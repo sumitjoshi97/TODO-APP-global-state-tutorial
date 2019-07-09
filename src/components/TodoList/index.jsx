77c48ea6f096136a76f615239ffe9f3dd0c7504d
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-07-09 15:41:58 +0530

    chore: render todos from context state

--- src/components/TodoList/index.jsx
+++ src/components/TodoList/index.jsx
@@ -1,5 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import Todo from './Todo'
+import { Store } from '../../store/Store'
 
 export default function TodoList() {
-  return <div>todoList</div>
+  const { state, dispatch } = useContext(Store)
+
+  const todoList = state.todos.map(todo => (
+    <Todo
+      key={todo.id}
+      todoId={todo.id}
+      todoText={todo.text}
+      todoTag={todo.tag}
+      dispatch={dispatch}
+    />
+  ))
+
+  return <div className="todo-list">{todoList}</div>
 }
