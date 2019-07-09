35665152968ab27745b9e18a4cea5f10cea311c0
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-07-09 15:40:52 +0530

    chore: remove context

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import TodoForm from './components/TodoForm'
 import TodoList from './components/TodoList'
-import { Store } from './store/Store'
+
 import './App.css'
 
 export default function App() {
-  const { state, dispatch } = useContext(Store)
   return (
     <div className="App">
       <TodoForm />
