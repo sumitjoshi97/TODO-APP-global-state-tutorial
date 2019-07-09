f5beac5f9fae910170b5d23bf215c22ccbd69c46
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-07-09 15:42:19 +0530

    chore: add todo component

---
+++ src/components/TodoList/Todo.jsx
@@ -0,0 +1,31 @@
+import React, { memo } from 'react'
+import { editTag, removeTodo } from '../../store/Actions'
+
+const Todo = memo(function Todo(props) {
+  const { todoId, todoText, todoTag, dispatch } = props
+
+  const handleTodoTag = () => {
+    const newTag = todoTag === 'active' ? 'complete' : 'active'
+    editTag(todoId, newTag, dispatch)
+  }
+
+  const handleRemoveTodo = () => {
+    removeTodo(todoId, dispatch)
+  }
+
+  const todoStyle = {
+    color: (todoTag === 'complete' && '#90a4ae') || '#01579b',
+    textDecoration: (todoTag === 'complete' && 'line-through') || 'none',
+  }
+
+  return (
+    <div className="todo">
+      <span style={todoStyle} onClick={handleTodoTag}>
+        {todoText}
+      </span>
+      <button onClick={handleRemoveTodo}>x</button>
+    </div>
+  )
+})
+
+export default Todo
