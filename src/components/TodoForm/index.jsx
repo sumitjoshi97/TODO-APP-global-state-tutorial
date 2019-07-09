4f663c87dcd8a2cb27e439ea066687c82e559126
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-07-09 15:41:23 +0530

    chore: add todo input form

--- src/components/TodoForm/index.jsx
+++ src/components/TodoForm/index.jsx
@@ -1,5 +1,25 @@
-import React from 'react'
+import React, { useState, useContext } from 'react'
+import { addTodo } from '../../store/Actions'
+import { Store } from '../../store/Store'
 
 export default function TodoForm() {
-  return <div>TodoForm</div>
+  const { dispatch } = useContext(Store)
+  const [todo, setTodo] = useState('')
+
+  const handleTodoInput = e => {
+    setTodo(e.target.value)
+  }
+
+  const handleTodoSubmit = e => {
+    e.preventDefault()
+    todo.trim().length > 0 && addTodo(todo, dispatch)
+    setTodo('')
+  }
+
+  return (
+    <form onSubmit={handleTodoSubmit} className="todo-form">
+      <input type="text" value={todo} onChange={handleTodoInput} />
+      <button type="submit">Add Todo</button>
+    </form>
+  )
 }
